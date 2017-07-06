a75e0b4f1590eb1ffc7c6f0ecb8dcf09c3a0af63
shellfie <calebshepard3@gmail.com>
2017-07-05 21:08:23 -0700

    Update tester.c
    xdddddddd

--- tester.c
+++ tester.c
@@ -5,35 +5,50 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cshepard <calebshepard@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/05 20:56:03 by cshepard          #+#    #+#             */
-/*   Updated: 2017/07/05 21:00:35 by cshepard         ###   ########.fr       */
+/*   Created: 2017/07/05 19:12:19 by cshepard          #+#    #+#             */
+/*   Updated: 2017/07/05 20:42:08 by cshepard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//compile this as well as your ft method and make sure that their outputs do the same thing
+
+#include <string.h>
 #include <stdio.h>
-#include <unistd.h>
-
-int ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (0);
-}
-
-void ft_swap(int *a, int *b)
-{
-	int tmp;
-
-	*tmp = a;
-	*a = b;
-	*b = tmp;
-}
 
 int main(void)
 {
-	int a = 5;
-	int b = 6;
-	printf("Before the swap, we have int a = %d and int b = %d\n", a, b);
-	ft_swap(&a, &b);
-	printf("After the swap, we have int a = %d and int b = %d\n", a, b);
-	return (0);
+	const char *s1 = "This string is 24 chars long";
+	const char *s2 = "This string is 42 chars long"; // not actually 42 chars long
+	const char *s3 = "A";
+	const char *s4 = "a";
+	const char *s5 = "b";
+	const char *s6 = "Yi";
+	const char *s7 = "Yo";
+	const char *s8 = "     ";
+	const char *s9 = "  ";
+	const char *s10 = "2435tws";
+	const char *s11 = "wertw short.";
+	const char *s12 = "Hello World!";
+	const char *s13 = "ba";
+
+	printf("%s\t%s\t%d\n", s1, s2, strcmp(s1, s2));
+	printf("%s\t%s\t%d\n", s1, s3, strcmp(s1, s3));
+	printf("%s\t%s\t%d\n", s1, s1, strcmp(s1, s1));
+	printf("%s\t%s\t%d\n", s1, s4, strcmp(s1, s4));
+	printf("%s\t%s\t%d\n", s1, s5, strcmp(s1, s5));
+	printf("%s\t%s\t%d\n", s1, s6, strcmp(s1, s6));
+	printf("%s\t%s\t%d\n", s1, s7, strcmp(s1, s7));
+	printf("%s\t%s\t%d\n", s1, s8, strcmp(s1, s8));
+	printf("%s\t%s\t%d\n", s1, s9, strcmp(s1, s9));
+	printf("%s\t%s\t%d\n", s1, s10, strcmp(s1, s10));
+	printf("%s\t%s\t%d\n", s1, s11, strcmp(s1, s11));
+	printf("%s\t%s\t%d\n", s1, s12, strcmp(s1, s12));
+	printf("%s\t%s\t%d\n", s6, s7, strcmp(s6, s7));
+	printf("%s\t%s\t%d\n", s8, s9, strcmp(s8, s9));
+	printf("%s\t%s\t%d\n", s3, s4, strcmp(s3, s4));
+	printf("%s\t%s\t%d\n", s4, s5, strcmp(s4, s5));
+	printf("%s\t%s\t%d\n", s11, s12, strcmp(s11, s12));
+	printf("%s\t%s\t%d\n", s4, s13, strcmp(s4, s13));
+
+	return 0;
 }
